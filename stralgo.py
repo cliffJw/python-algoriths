be3b6cc9679055d5c03dee3f64b33299ab2b99e1
cliffJw <42861304+cliffJw@users.noreply.github.com>
2021-02-07 13:29:41 -0500

    Update stralgo.py

--- stralgo.py
+++ stralgo.py
@@ -1,4 +1,5 @@
-def sol(S, B, cnt=0):
+#This is the initial code
+"""def sol(S, B, cnt=0):
     S = list(S)
     B = list(B)
     for i in B:
@@ -11,7 +12,20 @@ def sol(S, B, cnt=0):
                 return sol(S, B, cnt) 
             else:
                 return sol(S, B, cnt)
-    print(cnt)       
+    print(cnt) """
+#This is the updated version on feb 7 2021
+def sol(S, count=0):
+    B = "BALLOON"
+    for a in S:
+        if a in B:
+            S = S.replace(a, "", 1)
+            B = B.replace(a, "", 1)
+            print(S, B)
+            if B == "":
+                count += 1
+                return sol(S, count)
+
+    return count
     
 sol("BAONXXOLL", "BALLOON")         
 sol("BAOOLAONLNNOLOLLBLOGBAX", "BALLOON")
