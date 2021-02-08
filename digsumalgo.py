0ddb0da9830a2a00d786b87b57a9e1e2290a427f
cliffJw <42861304+cliffJw@users.noreply.github.com>
2021-02-08 17:12:13 +0300

    Update digsumalgo.py

--- digsumalgo.py
+++ digsumalgo.py
@@ -1,18 +1,15 @@
 def soln(N):
     num = []
-    return digit(N, num)
-    
-def digit(N, num):
-    if N in range(1, 10):
-        num.append(N)
-        num.sort()
-        number = int(''.join(map(str, num)))
-        return number
-    else:
-        num.append(9)
-        num.sort()
-        N -= 9
-        return digit(N, num)
+    while N > 0:
+        if (1<=N<=9):
+            num.append(N)
+            num.sort()
+            N = 0
+            number = int(''.join(map(str, num)))
+        else:
+            num.append(9)
+            N -= 9
+    return number
     
 
 print(soln(22))
