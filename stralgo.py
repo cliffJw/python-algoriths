f2f046b6a21c29355abfe28aef2c52747b35848a
cliffJw <42861304+cliffJw@users.noreply.github.com>
2021-02-08 14:40:55 +0300

    Update stralgo.py

--- stralgo.py
+++ stralgo.py
@@ -1,18 +1,3 @@
-#This is the initial code
-"""def sol(S, B, cnt=0):
-    S = list(S)
-    B = list(B)
-    for i in B:
-        if i in S:
-            S.remove(i)
-            B.remove(i)
-            if len(B)==0:
-                cnt += 1
-                B = list("BALLOON")
-                return sol(S, B, cnt) 
-            else:
-                return sol(S, B, cnt)
-    print(cnt) """
 #This is the updated version on feb 7 2021
 def sol(S, count=0):
     B = "BALLOON"
