121a8cc8830736d2876e25ac7140503a404473a5
cliffJw <42861304+cliffJw@users.noreply.github.com>
2021-02-08 19:24:26 +0300

    Update roadalgo.py

--- roadalgo.py
+++ roadalgo.py
@@ -1,17 +1,14 @@
-def solution(X, W, cnt=0):
-    X.sort()
-    v = X.copy()
-    if len(X) != 0:
-        for a in range(X[0], (X[0] + W + 1)):
-            while a in v:
-                v.remove(a)
-            X = v
-        cnt += 1
-        return solution(X, W, cnt)
-    else:
-        return cnt 
+def solution(X, Y, W):
+    count = 0
+    while len(X) > 0:
+        X.sort()
+        for a in range(X[0], X[0]+W+1):
+            while a in X:
+                X.remove(a)
+        count += 1
+    return count
 
 
-print(solution([2, 4, 2, 6, 7, 1], 2))
-print(solution([4, 8, 2, 2, 1, 4], 3))
-print(solution([0, 3, 6, 5], 1))
+print(solution([2, 4, 2, 6, 7, 1], [], 2))
+print(solution([4, 8, 2, 2, 1, 4], [], 3))
+print(solution([0, 3, 6, 5], [], 1))
