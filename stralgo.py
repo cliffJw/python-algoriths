e4565eacc76f2f852d26672b05739aa3e2819db7
cliffJw <42861304+cliffJw@users.noreply.github.com>
2021-02-08 15:54:47 +0300

    Update stralgo.py

--- stralgo.py
+++ stralgo.py
@@ -1,14 +1,15 @@
 #This is the updated version on feb 7 2021
 def sol(S, count=0):
+count = 0
     B = "BALLOON"
-    for a in S:
-        if a in B:
-            S = S.replace(a, "", 1)
-            B = B.replace(a, "", 1)
-            print(S, B)
-            if B == "":
-                count += 1
-                return sol(S, count)
+    while len(S) >= len(B):
+        for a in S:
+            if a in B:
+                S = S.replace(a, "", 1)
+                B = B.replace(a, "", 1)
+                if B == "":
+                    count += 1
+                    B = "BALLOON"
 
     return count
     
